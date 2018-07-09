d25f8da22ae77ecd596d468c5faaa9974d34f9f2
Earvin Gemenez <earvin.gemenez@gmail.com>
2018-07-09 11:33:40 +0800

    added blog

---
+++ blogs/models.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.db import models
+
+
+class Post(models.Model):
+    """ blog post
+    """
+    title = models.CharField(max_length=250)
+    slug = models.CharField(max_length=300, null=True, blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    tags = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.title}"
