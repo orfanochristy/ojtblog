04d4548b815bc5428c342c59354e182217f9cbe8
Kim <kimclarizzeremolta@gmail.com>
2018-07-10 17:17:57 +0800

    initial commit

---
+++ blogs/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+     
+urlpatterns = [
+    path('createpost/', views.createpost, name='createpost'),
+    path('index/', views.index, name='index'),
+    #path('editpost/<int:post_id>/', views.editpost, name='editpost'),
+]
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
+from .models import Post
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your views here.
+def index(request):
+	context = {}
+	context['posts'] = Post.objects.all()
+	context['name'] = 'Posts'
+	return render(request, 'blogs/index.html', context)
+	#return render(request, 'blogs/index.html')
+
+def createpost(request):
+	form = PostForm()
+	if request.method == "POST":
+		form = PostForm(data=request.POST)
+		if form.is_valid():
+			form = form.save(commit=False)
+			form.author = request.user
+			form.date_created = timezone.now()
+			form.save()
+			return HttpResponseRedirect(reverse('index'))
+		else:
+			return render(request, 'blogs/createpost.html', {"form": form})
+	else:
+		return render(request, 'blogs/createpost.html', {"form": form})
+	
+
+	#return render(request, 'blogs/createpost.html', {'form': form})
+		#form = Post()
