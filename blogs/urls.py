4b39e6070f61e8c0b9a922eeca59bff53f5cefc4
Christy <orfanochristy98@gmail.com>
2018-07-13 12:15:52 +0800

    search q

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-<<<<<<< HEAD
      
 urlpatterns = [
     path('createpost/', views.createpost, name='createpost'),
--- blogs/views.py
+++ blogs/views.py
@@ -8,38 +8,38 @@ from django.utils import timezone
 from django.db.models import Q
 
 def index(request):
-	context = {}
-	context['posts'] = Post.objects.filter(archive=False).order_by('-date_updated')
-	context['name'] = 'Posts'
-	return render(request, 'blogs/index.html', context)
+    context = {}
+    context['posts'] = Post.objects.filter(archive=False).order_by('-date_updated')
+    context['name'] = 'Posts'
+    return render(request, 'blogs/index.html', context)
 
 def createpost(request):
-	form = PostForm()
-	if request.method == "POST":
-		form = PostForm(data=request.POST)
-		if form.is_valid():
-			form = form.save(commit=False)
-			form.author = request.user
-			form.date_created = timezone.now()
-			form.save()
-			return HttpResponseRedirect(reverse('index'))
-		else:
-			return render(request, 'blogs/createpost.html', {"form": form})
-	else:
-		return render(request, 'blogs/createpost.html', {"form": form})
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(data=request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.author = request.user
+            form.date_created = timezone.now()
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, 'blogs/createpost.html', {"form": form})
+    else:
+        return render(request, 'blogs/createpost.html', {"form": form})
 
 def editpost(request, post_id):
-	template = 'blogs/editpost.html'
-	post = get_object_or_404(Post, id=post_id)
-	if request.method == 'POST':
-		form = PostForm(request.POST, instance = post)
-		if form.is_valid():
-			form.save()
-			return HttpResponseRedirect(reverse('index'))
-	else:
-		form =PostForm(instance=post)
-		args = {'form':form}
-		return render(request, 'blogs/editpost.html', args)
+    template = 'blogs/editpost.html'
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance = post)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form =PostForm(instance=post)
+        args = {'form':form}
+        return render(request, 'blogs/editpost.html', args)
 
 
 def post_details(request, post_id):
@@ -58,11 +58,14 @@ def search(request):
     
     if 'q' in request.GET:
         q = request.GET['q']
-        posts = Post.objects.filter( Q(title__icontains=q) | Q(tags__icontains=q) | Q(content__icontains=q))
-        return render(request, 'blogs/search_results.html', {'posts':posts, 'query':q}) 
+        if not q:
+            error = True
+        else:
+            posts = Post.objects.filter( Q(title__icontains=q) | Q(tags__icontains=q) | Q(content__icontains=q))
+            return render(request, 'blogs/search_results.html', {'posts':posts, 'query':q}) 
     
     
-    return render(render, 'blogs/index.html', {'error': error})
+    return render(request, 'blogs/index.html', {'error': error})
 
 def search_result(request, post_id):
     template = 'blogs/search_result.html'
@@ -73,21 +76,21 @@ def search_result(request, post_id):
     return render(request, template, context)
 
 def archivepost(request, post_id):
-	post = get_object_or_404(Post, id=post_id, author=request.user)
-	post.archive=True
-	post.save()
-	return HttpResponseRedirect(reverse('index'))
+    post = get_object_or_404(Post, id=post_id, author=request.user)
+    post.archive=True
+    post.save()
+    return HttpResponseRedirect(reverse('index'))
 
 def unarchivepost(request, post_id):
-	post = get_object_or_404(Post, id=post_id, author=request.user)
-	post.archive=False
-	post.save()
-	return HttpResponseRedirect(reverse('archivelist'))
+    post = get_object_or_404(Post, id=post_id, author=request.user)
+    post.archive=False
+    post.save()
+    return HttpResponseRedirect(reverse('archivelist'))
 
 def archivelist(request):
-	context = {}
-	context['posts'] = Post.objects.filter(archive=True)
-	context['name'] = 'Posts'
-	return render(request, 'blogs/archivelist.html', context)
+    context = {}
+    context['posts'] = Post.objects.filter(archive=True)
+    context['name'] = 'Posts'
+    return render(request, 'blogs/archivelist.html', context)
 
 
